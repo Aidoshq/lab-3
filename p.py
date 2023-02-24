c93675fcd1a9033da3db9618f4cb5f161465edcf
Aidana <mukhadilovaaidana@gmail.com>
2023-02-24 12:27:44 +0600

    ex

---
+++ p.py
@@ -0,0 +1,12 @@
+#3 task for classes
+
+class Rectangle():
+    def __init__(self,le,wi):
+        self.length = le
+        self.width = wi
+
+        def rectangle_area(self):
+            return self.length*self.width
+        
+        newRectangle = Rectangle(11 , 12)
+        print(newRectangle.rectangle_area())
